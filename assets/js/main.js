edd3fda58bd1975b2088c2f716ef7776c027b3d0
hani <hani.sulaiman.2002@gmail.com>
2023-04-03 14:43:44 +0300

    update 0008

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,11 +25,16 @@ let prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
   let currentScrollPos = window.pageYOffset;
   var navbar = document.querySelector(".nav");
+  if(window.pageYOffset<100){
+    navbar.classList.remove('scrolled-up');
+    navbar.classList.add("nav-transparent");
+  }
   if (prevScrollpos >= currentScrollPos) {
     // When the user scrolls up, hide the navigation bar
     navbar.classList.remove("scrolled-down");
     navbar.classList.add("scrolled-up");
-  } else {
+  } else if(prevScrollpos < currentScrollPos) {
+    navbar.classList.remove("nav-transparent");
     // When the user scrolls down, show the navigation bar
     navbar.classList.add("scrolled-down");
     navbar.classList.remove("scrolled-up");
