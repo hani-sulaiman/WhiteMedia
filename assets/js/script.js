2bcca60f508542e801ffdafec0bb45b4d849ede8
hani <hani.sulaiman.2002@gmail.com>
2023-04-08 20:37:27 +0300

    update 00021

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,12 @@ function includeJS (incFile) {
 }
 stopVideos();
 window.onload=()=>{
+    document.querySelector('.logo').classList.add('zoom');
     setTimeout(()=>{
         document.querySelector('.body').classList.remove('hide');
         document.querySelector('.load').classList.add('hide');
+    },800);   
+    setTimeout(()=>{
         Main();
         MainGallery();
         MainSlider();  
