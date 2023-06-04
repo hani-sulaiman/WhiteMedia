7a7d8f156f91376222e044ff265d601194215424
hani-sulaiman <123769193+hani-sulaiman@users.noreply.github.com>
2023-06-04 15:39:36 +0300

    Delete assets directory

--- assets/js/gallery.js
+++
@@ -1,3 +0,0 @@
-if (document.querySelector(".gallery")) {
-  lightGallery(document.querySelector(".gallery"));
-}
--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-  document.querySelector(".logo").classList.add("zoom");
-  setTimeout(() => {
-    document.querySelector(".body").classList.remove("hide");
-    document.querySelector(".load").classList.add("hide");
-  }, 800);
-};
